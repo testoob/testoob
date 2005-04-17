c10b6f425e348494b05a241c2d981cc86b1d6393
orip <>
2005-04-17 00:38:07 +0000

    restructured into class OpenSVNBackups

--- logistics/svn_backup.py
+++ logistics/svn_backup.py
@@ -1,36 +1,57 @@
 #!/usr/bin/python
-import urllib2, cookielib
-from urllib2 import urlopen
-from ClientForm import ParseResponse
 
-cookie_jar = cookielib.CookieJar()
-opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookie_jar))
+class LoginFailedError(RuntimeError): pass
 
-def choose_form(action, forms):
-    for form in forms:
-        if form["action"] == action:
-            return form
+class OpenSVNBackups:
+    def __init__(self, projectname):
+        self.projectname = projectname
+        self._opener = None
 
-def login(projectname, password):
-    forms = ParseResponse(opener.open("https://opensvn.csie.org"))
-    form = choose_form("login", forms)
-    form["project"] = projectname
-    form["password"] = password
+    def opener(self):
+        if self._opener is None:
+            self._opener = self.new_opener()
+        return self._opener
 
-    opener.open(form.click())
+    def open(self, url):
+        return self.opener().open(url)
 
-def backup_svn(r1, r2):
-    "backup subversion repository from r1 to r2."
-    filename = "testoob-%s-%s.gz" % (r1, r2)
-    url = "https://opensvn.csie.org/getbackup.pl/" + filename
-    open(filename, "wb").write( opener.open(url).read() )
+    def new_opener(self):
+        from cookielib import CookieJar
+        from urllib2 import build_opener, HTTPCookieProcessor
+        return build_opener(HTTPCookieProcessor(CookieJar()))
 
-PROJECT = "testoob"
-PASSWORD = "XXXXXXXX"
+    def choose_form(self, action, forms):
+        for form in forms:
+            if form["action"] == action:
+                return form
+        
+    def login(self, password):
+        from ClientForm import ParseResponse
+        forms = ParseResponse(self.open("https://opensvn.csie.org"))
+        form = self.choose_form("login", forms)
+        form["project"] = self.projectname
+        form["password"] = password
 
-print "logging in...",
-login(PROJECT, PASSWORD)
-print "done."
-print "saving subversion repository...",
-backup_svn(1, 5)
-print "done."
+        result = self.open(form.click()).read()
+        if result == 'login failed':
+            raise LoginFailedError()
+
+    def save_url(self, url, filename):
+        open(filename, "wb").write( self.open(url).read() )
+
+    def backup_svn(self, r1, r2):
+        "backup subversion repository from r1 to r2."
+        filename = "%s-%s-%s.gz" % (self.projectname, r1, r2)
+        url = "https://opensvn.csie.org/getbackup.pl/" + filename
+        self.save_url(url, filename)
+
+    def backup_trac(self):
+        filename = "%s-trac.tgz" % self.projectname
+        url = "https://opensvn.csie.org/getbackup.pl/%s?action=trac" % filename
+        self.save_url(url, filename)
+
+def backup(projectname, password):
+    backups = OpenSVNBackups(projectname)
+    backups.login(password)
+    backups.backup_svn(1, 6)
+    backups.backup_trac()
