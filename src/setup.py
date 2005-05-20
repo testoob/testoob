fff756956443f4b0ee3c6d5258b90f3dc63279c8
orip <>
2005-05-20 13:55:14 +0000

    Filled out disutil's and pypi's meta-data

--- src/setup.py
+++ src/setup.py
@@ -1,8 +1,58 @@
+
+kwargs = {
+    'packages' : ['testoob'],
+
+    # meta-data
+    'name'             : 'testoob',
+    'version'          : '0.1',
+    'author'           : 'Ori Peleg',
+    'author_email'     : 'testoob@gmail.com',
+    'url'              : 'http://testoob.sourceforge.net',
+    'license'          : 'LGPL',
+    'platforms'        : ['any'],
+    'description'      : 'TestOOB - An advanced unit testing framework',
+}
+
+
+kwargs['long_description'] = """
+TestOOB - Testing Out Of (The) Box
+
+TestOOB is a bundle of extensions to Python's built-in unit
+testing framework (module unittest).
+
+It provides advanced features, like easy and extensible filtering
+and reporting options.
+
+It can be used as a drop-in replacement for unittest: simply
+replace unittest.main() with testoob.main()!
+
+Even more options are available if you use testoob in your tests.
+See the homepage for details!
+""".strip()
+
+kwargs['classifiers'] = """
+Development Status :: 4 - Beta
+Environment :: Console
+Intended Audience :: Developers
+License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)
+Operating System :: OS Independent
+Operating System :: Microsoft :: Windows
+Operating System :: POSIX
+Programming Language :: Python
+Topic :: Software Development :: Quality Assurance
+Topic :: Software Development :: Testing
+""".strip().splitlines()
+
+# ============================================================================
+
+# patch distutils if it can't cope with the "classifiers" or
+# "download_url" keywords
+import sys
+if sys.version < '2.2.3':
+    from distutils.dist import DistributionMetadata
+    DistributionMetadata.classifiers = None
+    DistributionMetadata.download_url = None
+
 from distutils.core import setup
-setup(name         = 'testoob',
-      version      = '0.1',
-      author       = 'Ori Peleg',
-      author_email = 'testoob@gmail.com',
-      url          = 'http://testoob.sourceforge.net',
-      packages     = ['testoob'],
-     )
+
+setup(**kwargs)
