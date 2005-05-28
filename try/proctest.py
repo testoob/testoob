a79e1cad69e39c5e1fc9bf34af8b8051417486a5
orip <>
2005-05-28 00:32:22 +0000

    multiple objects sent to children, not perfect yet

--- try/proctest.py
+++ try/proctest.py
@@ -1,25 +1,39 @@
 # preliminary tests in sending pickled objects to/from parents and children
 
-import os, pickle, random
+import os, pickle, random, time, select, StringIO
 
 def child(to_parent, from_parent):
-    n = pickle.loads(os.read(from_parent, 10000))
-    os.close(from_parent)
+    n = 0
+    while n != 666:
+        # TODO: this hanges, because this reads the entire stream at once.
+        # This stream should be unpickled one-by-one.
+        # Possible workarounds:
+        # - send a sequence
+        # - embed markers and read upto the marker
+        # - maintain a StringIO=like object that can be read from _and_ written
+        #   to
+        n = pickle.loads(os.read(from_parent, 10000))
 
-    d = {
-        "pid" : os.getpid(),
-        "n": n,
-        "double" : n*2,
-    }
-    
-    os.write(to_parent, pickle.dumps(d))
+        d = {
+            "pid" : os.getpid(),
+            "n": n,
+            "double" : n*2,
+        }
+        
+        os.write(to_parent, pickle.dumps(d))
+
+    print "[%d] done" % os.getpid()
+    os.write(to_parent, pickle.dumps(666))
+    os.close(from_parent)
     os.close(to_parent)
     raise SystemExit
 
-def parent_read(fd):
+def parent_read(fd, fds_left):
     d = pickle.loads(os.read(fd, 10000))
-    os.close(fd)
-    print os.getpid(), "[parent]", "[fd=%2d]"%fd, "[received %s]" % d
+    if d == 666:
+        fds_left.remove(fd)
+    else:
+        print os.getpid(), "[parent]", "[fd=%2d]"%fd, "[received %s]" % d
 
 def spawn_child():
     from_child, to_parent = os.pipe()
@@ -29,28 +43,51 @@ def spawn_child():
     else:
         return to_child, from_child
 
-NUM_CHILDREN=10
+NUM_CHILDREN=3
+
+def send_numbers(fds, val=None):
+    for fd in fds:
+        n = val or random.randint(1000,9999)
+        print "[parent] sending %d" % n
+        os.write(fd, pickle.dumps(n))
+
+def perform_poll(poll, fds_left):
+    for (fd, event) in poll.poll(1000):
+        if event in (select.POLLHUP, select.POLLNVAL):
+            fds_left.remove(fd)
+            poll.unregister(fd)
+        elif event in (select.POLLIN, select.POLLPRI):
+            parent_read(fd, fds_left)
+        else:
+            print "Unexpected poll event '%s', fd=%d" % (event, fd)
+            raise SystemExit
+
+def send_input(children_to_fds):
+    for i in xrange(3):
+        send_numbers(children_to_fds)
+    send_numbers(children_to_fds, 666)
+
+def poll_output(children_from_fds):
+    fds_left = children_from_fds[:]
+    
+    poll = select.poll()
+    for fd in fds_left: poll.register(fd)
+
+    while fds_left:
+        perform_poll(poll, fds_left)
+
+    print "fds_left:", fds_left
 
 if __name__ == "__main__":
     children_from_fds = []
+    children_to_fds = []
+
     for i in xrange(NUM_CHILDREN):
         to_child, from_child = spawn_child()
-        n = random.randint(1000,9999)
-        print "[parent] sending %d" % n
-        os.write(to_child, pickle.dumps(n))
-        os.close(to_child)
         children_from_fds.append(from_child)
-    
-    import select
-    poll = select.poll()
-    for fd in children_from_fds: poll.register(fd)
-    while children_from_fds:
-        for (fd, event) in poll.poll():
-            if event in (select.POLLHUP, select.POLLNVAL):
-                children_from_fds.remove(fd)
-            elif event in (select.POLLIN, select.POLLPRI):
-                parent_read(fd)
-            else:
-                print "Unexpected poll event '%s', fd=%d" % (event, fd)
-                raise SystemExit
+        children_to_fds.append(to_child)
 
+    send_input(children_to_fds)
+    poll_output(children_from_fds)
+
+    for fd in children_from_fds + children_to_fds: os.close(fd)
