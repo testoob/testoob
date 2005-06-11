71e0aa6719a4c7c9daad53cd16eab5c8a6eb05d3
orip <>
2005-06-11 20:56:40 +0000

    examples aren't needed anymore

--- src/testoob/__init__.py
+++ src/testoob/__init__.py
@@ -6,17 +6,3 @@ from main import *
 from running import *
 from extractors import *
 from misc import *
-
-###############################################################################
-# examples
-###############################################################################
-def examples(suite):
-    print "== filtered =="
-    text_run(suite, test_extractor = regex_extractor("Th"))
-
-    print "== sequential =="
-    text_run(suite)
-
-    print "== threaded =="
-    try: text_run(suite, runner_class=ThreadedRunner)
-    except Exception, e: print "Got error:", str(e)
