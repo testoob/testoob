57811d822a21e38dbef2572693f57076bce36c30
orip <>
2005-06-11 22:07:37 +0000

    first commit

---
+++ logistics/generate_release.py
@@ -0,0 +1,68 @@
+#! /usr/bin/env python
+
+import re
+
+def parse_args():
+    import optparse
+    parser = optparse.OptionParser("usage: %prog [options] version")
+
+    parser.add_option(
+        "--for-keeps", action="store_true",
+        help="Actually generate the release (default: make a dry run)")
+
+    options, args = parser.parse_args()
+    if len(args) != 1:
+        parser.error("no version given")
+
+    version = args[0]
+    if not re.match(r"^\d(\.\d)+[a-z]?$", version):
+        parser.error("bad version format")
+
+    options.version = version
+    return options
+
+_options_ = None
+def options():
+    "single instance of options"
+    global _options_
+    if _options_ is None:
+        _options_ = parse_args()
+    return _options_
+
+def dry_run():
+    return not options().for_keeps
+    
+def root_dir():
+    from os.path import normpath, join, dirname
+    return normpath(join(dirname(__file__), ".."))
+
+def get_command_output(cmd):
+    try:
+        # Cross-platform, requires Python 2.4
+        from subprocess import Popen, PIPE
+        return Popen(cmd.split(), stdout=PIPE).communicate()[0]
+    except ImportError:
+        # POSIX-only
+        import commands
+        return commands.getoutput(cmd)
+
+def run_command(cmd):
+    print "Running '%s'" % cmd
+    if dry_run(): return
+    os.system(cmd)
+
+def svn_info():
+    return get_command_output("svn info %s" % root_dir())
+
+def svn_url():
+    regexp = r"URL: (?P<url>(http|https|svn|file)://\S+)"
+    return re.search(regexp, svn_info()).group("url")
+
+def version():
+    return options().version
+
+def create_branch(
+
+print options()
+print root_dir()
+print svn_url()
