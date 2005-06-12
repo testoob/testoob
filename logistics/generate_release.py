78e193b5eed4f166e2d3a29e74218814d5dea97b
orip <>
2005-06-12 00:46:32 +0000

    still getting there

--- logistics/generate_release.py
+++ logistics/generate_release.py
@@ -18,19 +18,29 @@ def parse_args():
     parser = optparse.OptionParser()
 
     parser.add_option(
-        "--for-keeps", action="store_true",
-        help="Actually generate the release (default: make a dry run)")
+        "--root-dir",
+        help = "the project's root dir")
 
     parser.add_option(
-        "--version",
-        help="The release version")
+        "--for-keeps", action="store_true",
+        help="actually modify the repository")
+
+    parser.add_option(
+        "--release", metavar="VERSION",
+        help="generate a release")
+
+    parser.add_option(
+        "--update-changelog", action="store_true",
+        help="update the changelog")
 
     options, args = parser.parse_args()
 
-    if not options.version: parser.error("no version given")
-    version_re=r"^\d(\.\d)+[a-z]?$"
-    if not re.match(version_re, options.version):
-        parser.error("bad version format, should match " + version_re)
+    if not options.root_dir: parser.error("root dir missing")
+
+    if options.release:
+        version_re=r"^\d(\.\d)+[a-z]?$"
+        if not re.match(version_re, options.release):
+            parser.error("bad version format, should match " + version_re)
 
     return options
 
@@ -52,8 +62,7 @@ def norm_join(*args):
 
 def root_dir():
     "the project's root dir"
-    from os.path import normpath, join, dirname
-    return norm_join(dirname(__file__), "..")
+    return options().root_dir()
 
 def get_command_output(cmd):
     import commands
@@ -75,26 +84,26 @@ def get_field(line, field_index):
 def svn_info():
     return get_command_output("svn info %s" % root_dir())
 
-def svn_base_url():
+def base_url():
     regexp = r"URL: (?P<url>(http|https|svn|file)://\S+)/trunk"
     return re.search(regexp, svn_info()).group("url")
 
-def svn_trunk_url(): return svn_base_url() + "/trunk"
-def svn_branches_url(): return svn_base_url() + "/branches"
-def svn_tags_url(): return svn_base_url() + "/tags"
+def trunk_url(): return base_url() + "/trunk"
+def branches_url(): return base_url() + "/branches"
+def tags_url(): return base_url() + "/tags"
 
 @once
 def last_branch_revision():
-    branch_list = get_command_output("svn list -v %s" % svn_branches_url())
+    branch_list = get_command_output("svn list -v %s|grep '^RB-'" % branches_url())
     return get_field(tail(branch_list, 1), 0)
 
 def die(msg):
     raise RuntimeError(msg)
 
 def version():
-    return options().version
+    return options().release
 
-def svn_up_to_date():
+def up_to_date():
     return len(get_command_output("svn status -u|grep -v '^Status against revision'").strip()) == 0
 
 def replace_string(from_str, to_str, files):
@@ -108,7 +117,7 @@ def replace_version_string():
 def create_branch():
     os.chdir(root_dir())
     run_command("svn update")
-    if not svn_up_to_date(): die("problem updating with svn")
+    if not up_to_date(): die("problem updating with svn")
     update_changelog()
 
 def changelog():
@@ -118,6 +127,27 @@ def update_changelog():
     temp_changelog = "%s.temp" % changelog()
     run_command("mv %s %s" % (changelog(), temp_changelog))
     run_command("svn log -v -r%s:HEAD %s | cat - %s > %s" %
-            (last_branch_revision(), svn_trunk_url(), temp_changelog, changelog()))
+            (last_branch_revision(), trunk_url(), temp_changelog, changelog()))
+    run_command("rm -f %s" % temp_changelog)
 
-create_branch()
+def switch_to_branch():
+    run_command("svn switch %s %s" % (release_branch_url(), root_dir()))
+
+def switch_to_trunk():
+    run_command("svn switch %s %s" % (trunk_url(), root_dir()))
+
+def commit(msg):
+    run_command("svn commit %s -m '%s'" % (root_dir(), msg))
+
+if options().update_changelog:
+    update_changelog()
+
+elif options().release:
+    create_branch()
+    switch_to_branch()
+    replace_version_string()
+    commit("updated version string")
+    switch_to_trunk()
+
+else:
+    print >>sys.stderr, "Bad arguments, run with '-h' for usage"
