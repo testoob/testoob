c500bfd61e35b58d67b90519ae2c787ecc2fd80f
orip <>
2005-06-14 20:49:17 +0000

    moved ReporterWithMemory to helpers.py

--- tests/runningtests.py
+++ tests/runningtests.py
@@ -1,49 +1,13 @@
 import helpers
 helpers.fix_include_path()
 
-class ReporterWithMemory:
-    "A reporter that remembers info on the test fixtures run"
-    def __init__(self):
-        self.started = []
-        self.successes = []
-        self.errors = []
-        self.failures = []
-        self.finished = []
-        self.stdout = ""
-        self.stderr = ""
-
-        self.has_started = False
-        self.is_done = False
-
-    def _append_test(self, l, test):
-        l.append(str(test).split()[0])
-
-    def start(self):
-        self.has_started = True
-    def done(self):
-        self.is_done = True
-    def startTest(self, test):
-        self._append_test(self.started, test)
-    def stopTest(self, test):
-        self._append_test(self.finished, test)
-    def addError(self, test, err):
-        self._append_test(self.errors, test)
-    def addFailure(self, test, err):
-        self._append_test(self.failures, test)
-    def addSuccess(self, test):
-        self._append_test(self.successes, test)
-
-    def __str__(self):
-        attrs = ("started","successes","errors","failures","finished","stdout","stderr")
-        return "\n".join(["%s = %s" % (attr, getattr(self,attr)) for attr in attrs])
-
 import unittest
 import testoob.running
 import suites
 
 class TestoobBaseTestCase(unittest.TestCase):
     def setUp(self):
-        self.reporter = ReporterWithMemory()
+        self.reporter = helpers.ReporterWithMemory()
     def tearDown(self):
         del self.reporter
     def _check_reporter(self, **kwargs):
