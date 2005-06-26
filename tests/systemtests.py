97ac213e4c02312712af04ebee2f301f3383c7c5
orip <>
2005-06-26 14:50:03 +0000

    preliminary system tests, also used to flesh out the command-line testing facilities

--- tests/systemtests.py
+++ tests/systemtests.py
@@ -1,7 +1,20 @@
-import unittest
+import helpers
+helpers.fix_include_path()
+
+import unittest, testoob
 
 class CommandLineTestCase(unittest.TestCase):
-    pass
+    def testSuccesfulRun(self):
+        cmd = "python ../src/testoob/testoob suites.py Casedigits"
+        expected_output_regex = r"""
+\.\.\.\.\.\.\.\.\.\.
+----------------------------------------------------------------------
+Ran 10 tests in 0.0\d+s
+OK
+""".strip()
+
+        testoob.testing.command_line(
+            cmd=cmd, expected_output_regex=expected_output_regex)
 
 def suite(): return unittest.makeSuite(CommandLineTestCase)
 if __name__ == "__main__": unittest.main()
