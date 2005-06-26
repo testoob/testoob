c0a0be58b09a81239a63feb65e95fdc1284b96e9
orip <>
2005-06-26 13:50:29 +0000

    extracted imported_test_modules function

--- tests/alltests.py
+++ tests/alltests.py
@@ -1,22 +1,22 @@
+from __future__ import generators # Python 2.2 friendly
+
 import unittest
 from types import ModuleType
 
 import runningtests
 import extractingtests
 
+def imported_test_modules():
+    for val in globals().values():
+        if type(val) == ModuleType and \
+           hasattr(val, "suite") and \
+           callable(val.suite):
+               yield val
+
 def suite():
     result = unittest.TestSuite()
-
-    [
-        result.addTest(val.suite())
-
-        for val in globals().values()
-
-        if type(val) == ModuleType and
-           hasattr(val, "suite") and
-           callable(val.suite)
-    ]
-
+    for module in imported_test_modules():
+        result.addTest(module.suite())
     return result
 
 if __name__ == "__main__": unittest.main(defaultTest="suite")
