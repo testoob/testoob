12e53dc9dfab3a4910cc1b2ce21ea01dbd3f18f5
orip <>
2005-06-28 23:53:15 +0000

    docbook website no longer needed

--- web/docbook_website/dynxbel.js
+++
@@ -1,8 +0,0 @@
-function toggleList(list) {
-  var display = eval("document.all." + list + ".style.display");
-  if (display == "none") {
-    eval("document.all." + list + ".style.display = \"\""); 
-  } else {
-    eval("document.all." + list + ".style.display = \"none\""); 
-  }
-}
