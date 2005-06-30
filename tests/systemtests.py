267da9d1089e91d5a1560dfc2210e04d437ed2a4
orip <>
2005-06-30 16:01:06 +0000

    using correct testoob program path, expecting stderr and not stdout

--- tests/systemtests.py
+++ tests/systemtests.py
@@ -3,10 +3,12 @@ helpers.fix_include_path()
 
 import unittest, testoob
 
+_suite_file = helpers.project_subpath("tests/suites.py")
+
 class CommandLineTestCase(unittest.TestCase):
     def testSuccesfulRun(self):
-        cmd = "python ../src/testoob/testoob suites.py Casedigits"
-        expected_output_regex = r"""
+        cmd = "python %s %s CaseDigits" % (helpers.executable_path(), _suite_file)
+        expected_error_regex = r"""
 \.\.\.\.\.\.\.\.\.\.
 ----------------------------------------------------------------------
 Ran 10 tests in 0.0\d+s
@@ -14,7 +16,7 @@ OK
 """.strip()
 
         testoob.testing.command_line(
-            cmd=cmd, expected_output_regex=expected_output_regex)
+            cmd=cmd, expected_error_regex=expected_error_regex)
 
 def suite(): return unittest.makeSuite(CommandLineTestCase)
 if __name__ == "__main__": unittest.main()
