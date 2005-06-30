1b0a3d282ac5fb358183ac520f664a86b79edd70
orip <>
2005-06-30 14:37:59 +0000

    implemented function command_line, added functions assert_equals and assert_matches, fixed _run_command_* default input parameter

--- src/testoob/testing.py
+++ src/testoob/testing.py
@@ -1,8 +1,8 @@
-def _run_command_subprocess(cmd, input):
+def _run_command_subprocess(cmd, input=None):
     import subprocess
     raise NotImplementedError
 
-def _run_command_popen2(cmd, input):
+def _run_command_popen2(cmd, input=None):
     import popen2
     child = popen2.Popen3(cmd, capturestderr=True)
 
@@ -30,11 +30,47 @@ def _run_command(cmd, input=None):
 if _has_subprocess_module(): _run_command = _run_command_subprocess
 else:                        _run_command = _run_command_popen2
 
+def assert_equals(expected, actual, msg = None):
+    "works like unittest.TestCase.assertEquals"
+    if expected == actual: return
+    if msg is None:
+        msg = '%s != %s' % (expected, actual)
+    raise AssertionError(msg)
+
+def assert_matches(regex, actual, msg=None):
+    "fail unless regex matches actual (using re.search)"
+    import re
+    if re.search(regex, actual) is not None: return
+
+    if msg is None:
+        msg = "'%(actual)s' doesn't match regular expression '%(regex)s'" % vars()
+    raise AssertionError(msg)
+
+def conditionally_assert_equals(expected, actual):
+    "assert_equals only if expected is not None"
+    if expected is not None:
+        assert_equals(expected, actual)
+
+def conditionally_assert_matches(expected_regex, actual):
+    "assert_matches only if expected_regex is not None"
+    if expected_regex is not None:
+        assert_matches(expected_regex, actual)
+
 def command_line(
         cmd,
         input=None,
         expected_output=None,
         expected_error=None,
         expected_output_regex=None,
-        expected_error_regex=None,):
-    raise NotImplementedError
+        expected_error_regex=None,
+        expected_rc=None):
+
+    # run command
+    output, error, rc = _run_command(cmd, input)
+
+    # test
+    conditionally_assert_equals(expected_output, output)
+    conditionally_assert_matches(expected_output_regex, output)
+    conditionally_assert_equals(expected_error, error)
+    conditionally_assert_matches(expected_error_regex, error)
+    conditionally_assert_equals(expected_rc, rc)
