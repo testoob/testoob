490f0eccb32aa04f93396df1e8b0d53d7b540f66
orip <>
2005-07-02 22:07:10 +0000

    slightly different output on windows, fixed test suite

--- tests/systemtests.py
+++ tests/systemtests.py
@@ -38,16 +38,16 @@ OK$
     def testSuccesfulRunVerbose(self):
         args = _create_args(options=["-v"], testcase="CaseDigits")
         regex = r"""
-test0 \(suites\.CaseDigits\.test0\) \.\.\. ok
-test1 \(suites\.CaseDigits\.test1\) \.\.\. ok
-test2 \(suites\.CaseDigits\.test2\) \.\.\. ok
-test3 \(suites\.CaseDigits\.test3\) \.\.\. ok
-test4 \(suites\.CaseDigits\.test4\) \.\.\. ok
-test5 \(suites\.CaseDigits\.test5\) \.\.\. ok
-test6 \(suites\.CaseDigits\.test6\) \.\.\. ok
-test7 \(suites\.CaseDigits\.test7\) \.\.\. ok
-test8 \(suites\.CaseDigits\.test8\) \.\.\. ok
-test9 \(suites\.CaseDigits\.test9\) \.\.\. ok
+test0 \(.*suites\.CaseDigits\.test0\) \.\.\. ok
+test1 \(.*suites\.CaseDigits\.test1\) \.\.\. ok
+test2 \(.*suites\.CaseDigits\.test2\) \.\.\. ok
+test3 \(.*suites\.CaseDigits\.test3\) \.\.\. ok
+test4 \(.*suites\.CaseDigits\.test4\) \.\.\. ok
+test5 \(.*suites\.CaseDigits\.test5\) \.\.\. ok
+test6 \(.*suites\.CaseDigits\.test6\) \.\.\. ok
+test7 \(.*suites\.CaseDigits\.test7\) \.\.\. ok
+test8 \(.*suites\.CaseDigits\.test8\) \.\.\. ok
+test9 \(.*suites\.CaseDigits\.test9\) \.\.\. ok
 
 ----------------------------------------------------------------------
 Ran 10 tests in 0\.0\d+s
@@ -60,7 +60,7 @@ OK
         args = _create_args(options=["-q"], testcase="CaseFailure")
         regex = r"""
 ^======================================================================
-FAIL: testFailure \(suites\.CaseFailure\.testFailure\)
+FAIL: testFailure \(.*suites\.CaseFailure\.testFailure\)
 ----------------------------------------------------------------------
 .*AssertionError
 
@@ -74,9 +74,9 @@ FAILED \(failures=1\)$
     def testRegex(self):
         args = _create_args(options=["-v", "--regex=A|D|J"], testcase="CaseLetters")
         regex=r"""
-testA \(suites\.CaseLetters\.testA\) \.\.\. ok
-testD \(suites\.CaseLetters\.testD\) \.\.\. ok
-testJ \(suites\.CaseLetters\.testJ\) \.\.\. ok
+testA \(.*suites\.CaseLetters\.testA\) \.\.\. ok
+testD \(.*suites\.CaseLetters\.testD\) \.\.\. ok
+testJ \(.*suites\.CaseLetters\.testJ\) \.\.\. ok
 """.strip()
 
         testoob.testing.command_line(args=args, expected_error_regex=regex)
