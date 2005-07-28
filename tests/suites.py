89de094f000dd74ac6ba084d2b73cec3c9c8b938
orip <>
2005-07-28 09:59:38 +0000

    removed unused logged_run

--- tests/suites.py
+++ tests/suites.py
@@ -1,89 +1,62 @@
 import unittest
 
-class RunLog:
-    def __init__(self):
-        self.clear()
-    def clear(self):
-        self.started = []
-        self.successes = []
-        self.errors = []
-        self.failures = []
-        self.finished = []
-        self.stdout = ""
-        self.stderr = ""
-
-run_log = RunLog()
-
-def logged_run(name, func, *args, **kwargs):
-    run_log.started.append(name)
-
-    try:
-        func(*args, **kwargs)
-        run_log.successes.append(name)
-    finally:
-        run_log.finished.append(name)
-
-
 class CaseLetters(unittest.TestCase):
-    def testA(self): logged_run("testA", self.assertEquals, "A" + "A", "AA")
-    def testB(self): logged_run("testB", self.assertEquals, "B" + "B", "BB")
-    def testC(self): logged_run("testC", self.assertEquals, "C" + "C", "CC")
-    def testD(self): logged_run("testD", self.assertEquals, "D" + "D", "DD")
-    def testE(self): logged_run("testE", self.assertEquals, "E" + "E", "EE")
-    def testF(self): logged_run("testF", self.assertEquals, "F" + "F", "FF")
-    def testG(self): logged_run("testG", self.assertEquals, "G" + "G", "GG")
-    def testH(self): logged_run("testH", self.assertEquals, "H" + "H", "HH")
-    def testI(self): logged_run("testI", self.assertEquals, "I" + "I", "II")
-    def testJ(self): logged_run("testJ", self.assertEquals, "J" + "J", "JJ")
-    def testK(self): logged_run("testK", self.assertEquals, "K" + "K", "KK")
-    def testL(self): logged_run("testL", self.assertEquals, "L" + "L", "LL")
-    def testM(self): logged_run("testM", self.assertEquals, "M" + "M", "MM")
-    def testN(self): logged_run("testN", self.assertEquals, "N" + "N", "NN")
-    def testO(self): logged_run("testO", self.assertEquals, "O" + "O", "OO")
-    def testP(self): logged_run("testP", self.assertEquals, "P" + "P", "PP")
-    def testQ(self): logged_run("testQ", self.assertEquals, "Q" + "Q", "QQ")
-    def testR(self): logged_run("testR", self.assertEquals, "R" + "R", "RR")
-    def testS(self): logged_run("testS", self.assertEquals, "S" + "S", "SS")
-    def testT(self): logged_run("testT", self.assertEquals, "T" + "T", "TT")
-    def testU(self): logged_run("testU", self.assertEquals, "U" + "U", "UU")
-    def testV(self): logged_run("testV", self.assertEquals, "V" + "V", "VV")
-    def testW(self): logged_run("testW", self.assertEquals, "W" + "W", "WW")
-    def testX(self): logged_run("testX", self.assertEquals, "X" + "X", "XX")
-    def testY(self): logged_run("testY", self.assertEquals, "Y" + "Y", "YY")
-    def testZ(self): logged_run("testZ", self.assertEquals, "Z" + "Z", "ZZ")
+    def testA(self): self.assertEquals("A" + "A", "AA")
+    def testB(self): self.assertEquals("B" + "B", "BB")
+    def testC(self): self.assertEquals("C" + "C", "CC")
+    def testD(self): self.assertEquals("D" + "D", "DD")
+    def testE(self): self.assertEquals("E" + "E", "EE")
+    def testF(self): self.assertEquals("F" + "F", "FF")
+    def testG(self): self.assertEquals("G" + "G", "GG")
+    def testH(self): self.assertEquals("H" + "H", "HH")
+    def testI(self): self.assertEquals("I" + "I", "II")
+    def testJ(self): self.assertEquals("J" + "J", "JJ")
+    def testK(self): self.assertEquals("K" + "K", "KK")
+    def testL(self): self.assertEquals("L" + "L", "LL")
+    def testM(self): self.assertEquals("M" + "M", "MM")
+    def testN(self): self.assertEquals("N" + "N", "NN")
+    def testO(self): self.assertEquals("O" + "O", "OO")
+    def testP(self): self.assertEquals("P" + "P", "PP")
+    def testQ(self): self.assertEquals("Q" + "Q", "QQ")
+    def testR(self): self.assertEquals("R" + "R", "RR")
+    def testS(self): self.assertEquals("S" + "S", "SS")
+    def testT(self): self.assertEquals("T" + "T", "TT")
+    def testU(self): self.assertEquals("U" + "U", "UU")
+    def testV(self): self.assertEquals("V" + "V", "VV")
+    def testW(self): self.assertEquals("W" + "W", "WW")
+    def testX(self): self.assertEquals("X" + "X", "XX")
+    def testY(self): self.assertEquals("Y" + "Y", "YY")
+    def testZ(self): self.assertEquals("Z" + "Z", "ZZ")
 
 class CaseDigits(unittest.TestCase):
-    def test0(self): logged_run("test0", self.assertEquals, "0" + "0", "00")
-    def test1(self): logged_run("test1", self.assertEquals, "1" + "1", "11")
-    def test2(self): logged_run("test2", self.assertEquals, "2" + "2", "22")
-    def test3(self): logged_run("test3", self.assertEquals, "3" + "3", "33")
-    def test4(self): logged_run("test4", self.assertEquals, "4" + "4", "44")
-    def test5(self): logged_run("test5", self.assertEquals, "5" + "5", "55")
-    def test6(self): logged_run("test6", self.assertEquals, "6" + "6", "66")
-    def test7(self): logged_run("test7", self.assertEquals, "7" + "7", "77")
-    def test8(self): logged_run("test8", self.assertEquals, "8" + "8", "88")
-    def test9(self): logged_run("test9", self.assertEquals, "9" + "9", "99")
+    def test0(self): self.assertEquals("0" + "0", "00")
+    def test1(self): self.assertEquals("1" + "1", "11")
+    def test2(self): self.assertEquals("2" + "2", "22")
+    def test3(self): self.assertEquals("3" + "3", "33")
+    def test4(self): self.assertEquals("4" + "4", "44")
+    def test5(self): self.assertEquals("5" + "5", "55")
+    def test6(self): self.assertEquals("6" + "6", "66")
+    def test7(self): self.assertEquals("7" + "7", "77")
+    def test8(self): self.assertEquals("8" + "8", "88")
+    def test9(self): self.assertEquals("9" + "9", "99")
 
 class CaseMixed(unittest.TestCase):
-    def testSuccess(self): logged_run("testSuccess", lambda:None)
-    def testFailure(self): logged_run("testFailure", self.fail)
-    def testError(self):
-        def error(): raise RuntimeError
-        logged_run("testError", error)
+    def testSuccess(self): pass
+    def testFailure(self): self.fail()
+    def testError(self): raise RuntimeError
+
     def suite():
         return unittest.makeSuite(CaseMixed)
     suite = staticmethod(suite)
 
 class CaseFailure(unittest.TestCase):
-    def testFailure(self): logged_run("testFailure", self.fail)
+    def testFailure(self): self.fail()
     def suite():
         return unittest.makeSuite(CaseFailure)
     suite = staticmethod(suite)
 
 class CaseError(unittest.TestCase):
-    def testError(self):
-        def error(): raise RuntimeError
-        logged_run("testError", error)
+    def testError(self): raise RuntimeError
     def suite():
         return unittest.makeSuite(CaseError)
     suite = staticmethod(suite)
