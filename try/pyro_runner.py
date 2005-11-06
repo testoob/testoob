6f41625bd0dc16782a84ee7cce7efafb036195e0
orip <>
2005-11-06 20:23:27 +0000

    forking client and server from same process

--- try/pyro_runner.py
+++ try/pyro_runner.py
@@ -3,13 +3,16 @@ import Pyro
 import Pyro.core
 import Pyro.naming
 
+
+fixture_dict = {}
+
 class StrQueue:
 	def __init__(self):
 		self._queue = []
 		self._ready = False
 		self._done = False
 	def push(self, obj): self._queue.append(obj)
-	def pop(self):return self._queue.pop()
+	def pop(self):return str(self._queue.pop())
 	def empty(self): return len(self._queue) == 0
 	def ready(self): return self._ready
 	def mark_ready(self): self._ready = True
@@ -65,8 +68,45 @@ class PyroRunner(running.BaseRunner):
 		print "All done :-)" # XXX
 		running.BaseRunner.done(self)
 
-if __name__ == "__main__":
-	# sample code
+def client_code():
+	print "client started" # XXX
+	import time
+	time.sleep(3)
+	import Pyro.errors
+	Pyro.core.initClient()
+
+
+	locator = Pyro.naming.NameServerLocator()
+	ns = locator.getNS()
+
+	def get_uri():
+		while True:
+			try:
+				return ns.resolve(":testoob:test_queue")
+			except Pyro.errors.NamingError:
+				time.sleep(1)
+
+	print "client: waiting for queue registration"
+	uri = get_uri()
+	print "client: queue has been registered"
+
+	queue = uri.getProxy()
+
+	#from testoob import reporting
+	#reporter = reporting.TextStreamReporter(sys.stdout)
+
+	def wait_for_queue():
+		print "client: waiting for queue readiness"
+		while not queue.ready():
+			time.sleep(0.1)
+		print "client: queue is ready"
+	wait_for_queue()
+
+	while not queue.empty() and queue.done():
+		print "client:", queue.pop()
+
+def server_code():
+	print "server started" # XXX
 	import suite, sys
 	from testoob import reporting
 
@@ -74,3 +114,11 @@ if __name__ == "__main__":
 	reporter = reporting.TextStreamReporter(sys.stdout, 0, 0)
 
 	running.run_suites(suites=[suite], reporters=[reporter], runner=PyroRunner())
+
+if __name__ == "__main__":
+	import os
+	if os.fork() == 0:
+		# child
+		client_code()
+	else:
+		server_code()
