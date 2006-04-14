7800863d8c5c6a86dbb1ec5ace3991e4b01dfa2f
orip <>
2006-04-14 21:59:50 +0000

    reorganized test cases

--- tests/unit/test_coverage.py
+++ tests/unit/test_coverage.py
@@ -1,15 +1,18 @@
-"coverage unit tests"
+"coverage tests"
 
 from testoob import coverage
 from unittest import TestCase
 
 import coverage_sample_module as sample
 
-class tests(TestCase):
+class CoverageTest(TestCase):
     def setUp(self):
         self.coverage = coverage.Coverage()
     def tearDown(self):
         del self.coverage
+
+class unit_tests(CoverageTest):
+    "Small-grain tests for Coverage"
 
     sample_coverage_dictionary = {
         "one.py" : { "lines"   : range(100),
@@ -17,10 +20,6 @@ class tests(TestCase):
         "two.py" : { "lines"   : range(66),
                      "covered" : range(66) },
     }
-
-    def test_percentage_full(self):
-        self.coverage.runfunc(sample.foo, 5)
-        self.assertEqual( 100, self.coverage.total_coverage_percentage() )
 
     def test_total_lines(self):
         self.coverage.coverage = self.sample_coverage_dictionary.copy()
@@ -33,6 +32,13 @@ class tests(TestCase):
     def test_total_coverage_percentage(self):
         self.coverage.coverage = self.sample_coverage_dictionary.copy()
         self.assertEqual( 81, self.coverage.total_coverage_percentage() )
+
+class system_tests(CoverageTest):
+    "Large-grain tests for Coverage"
+
+    def test_percentage_full(self):
+        self.coverage.runfunc(sample.foo, 5)
+        self.assertEqual( 100, self.coverage.total_coverage_percentage() )
 
 if __name__ == "__main__":
     testoob.main()
