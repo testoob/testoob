bcf6f984b4e6a6a87e80f4ca6e09a6139914d72b
orip <>
2006-04-14 22:12:19 +0000

    Added some _should_cover_frame unit tests, and strange SimpleStub class :)

--- tests/unit/test_coverage.py
+++ tests/unit/test_coverage.py
@@ -5,6 +5,11 @@ from unittest import TestCase
 
 import coverage_sample_module as sample
 
+class SimpleStub:
+    def __getattr__(self, name):
+        setattr(self, name, SimpleStub())
+        return getattr(self, name)
+
 class CoverageTest(TestCase):
     def setUp(self):
         self.coverage = coverage.Coverage()
@@ -32,6 +37,24 @@ class unit_tests(CoverageTest):
     def test_total_coverage_percentage(self):
         self.coverage.coverage = self.sample_coverage_dictionary.copy()
         self.assertEqual( 81, self.coverage.total_coverage_percentage() )
+
+    def test_should_cover_frame_path_to_ignore(self):
+        from os.path import abspath
+        self.coverage.ignorepaths = [abspath("/a/b/c")]
+
+        mock_frame = SimpleStub()
+        mock_frame.f_code.co_filename = "/a/b/c/blah.py"
+
+        self.failIf( self.coverage._should_cover_frame(mock_frame) )
+
+    def test_should_cover_frame_nonexecutable_line(self):
+        self.coverage.coverage = self.sample_coverage_dictionary.copy()
+
+        mock_frame = SimpleStub()
+        mock_frame.f_code.co_filename = "one.py"
+        mock_frame.f_lineno = 1000 # out of the range of one.py's lines
+
+        self.failIf( self.coverage._should_cover_frame(mock_frame) )
 
 class system_tests(CoverageTest):
     "Large-grain tests for Coverage"
