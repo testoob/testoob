ae2e982af3bc313404d733afff054ad98307c6dc
orip <>
2006-04-22 19:21:17 +0000

    misc. text changes

--- src/testoob/reporting/pdf.py
+++ src/testoob/reporting/pdf.py
@@ -1,5 +1,5 @@
 # Testoob, Python Testing Out Of (The) Box
-# Copyright (C) 2005 The Testoob Team
+# Copyright (C) 2005-2006 The Testoob Team
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"PDF reported which uses reportlib's opensource pdf library"
+"PDF reporting using ReportLab's PDF library"
 
 from base import BaseReporter
 
