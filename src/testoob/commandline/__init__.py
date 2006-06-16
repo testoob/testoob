aa0437ff5a3ee72356c13ca7f00bb18bc083fea4
orip <>
2006-06-16 12:45:45 +0000

    Loading cmdline option plugins dynamically (searching in the current directory)

--- src/testoob/commandline/__init__.py
+++ src/testoob/commandline/__init__.py
@@ -1,8 +1,22 @@
-import xml_option
-import pdf_option
-import html_option
-import regex_option
-import list_option
-import processes_option
-import randomize_option
-import pbar_option
+import glob, os
+
+def module_name(path):
+    return os.path.splitext(os.path.basename(path))[0]
+
+def current_path():
+    return os.path.dirname(__file__)
+
+def option_file_glob_pattern():
+    return os.path.join(current_path(), "*_option.py")
+
+def module_list():
+    return [
+        module_name(filename)
+        for filename in glob.glob(option_file_glob_pattern())
+    ]
+
+def load_options():
+    for module in module_list():
+        exec "import %s" % module
+
+load_options()
