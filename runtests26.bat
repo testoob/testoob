4e9bcfbd41edeb9c00ced9ff14daff4dca81d96f
Ori Peleg <orip>
2008-10-25 22:31:20 +0200

    More test running helpers

---
+++ runtests26.bat
@@ -0,0 +1,3 @@
+set PYTHONPATH=src
+set TESTOOB_DEVEL_TEST=1
+c:\Python26\python.exe src\testoob\testoob tests\alltests.py suite %*
---
+++ runtests_ipy11.bat
@@ -0,0 +1,3 @@
+set PYTHONPATH=src
+set TESTOOB_DEVEL_TEST=1
+c:\IronPython-1.1.2\ipy.exe src\testoob\testoob tests\alltests.py suite %*
