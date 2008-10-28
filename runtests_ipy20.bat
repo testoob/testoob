6bd27245ae0304e1bf1280b90f018a9c00e71050
Ori Peleg <orip>
2008-10-28 02:20:18 +0200

    using Python 2.5 libs fir ipy 2.0

--- runtests_ipy20.bat
+++ runtests_ipy20.bat
@@ -1,3 +1,3 @@
-set IRONPYTHONPATH=src;c:\Python24\Lib
+set IRONPYTHONPATH=src;c:\Python25\Lib
 set TESTOOB_DEVEL_TEST=1
 c:\IronPython-2.0rc1\ipy.exe -v src\testoob\testoob tests\alltests.py suite %*
