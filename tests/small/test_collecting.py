df6b61130a15537f68452f2d5d41deaa1b992447
Ori Peleg <orip>
2008-10-28 02:50:41 +0200

    Cosmetics

--- tests/small/test_collecting.py
+++ tests/small/test_collecting.py
@@ -6,6 +6,7 @@ from testoob import collecting
 class frame_code(TestCase):
     def test_frame_filename(self):
         self.assertEqual(__file__, collecting._frame_filename(sys._getframe()))
+
     def test_first_external_frame(self):
         # we should get our own current frame
         self.assertEqual(sys._getframe(), collecting._first_external_frame())
