7c47f0cbe1abf81eb368d35227b9200cfd29b989
Ori Peleg <orip>
2008-11-15 18:33:40 +0200

    Batch file to run tests with different versions of CPython on Windows

---
+++ runtestsall.bat
@@ -0,0 +1,5 @@
+set PYTHONPATH=src
+set TESTOOB_DEVEL_TEST=1
+c:\Python26\python.exe src\testoob\testoob tests\alltests.py suite %*
+c:\Python25\python.exe src\testoob\testoob tests\alltests.py suite %*
+c:\Python24\python.exe src\testoob\testoob tests\alltests.py suite %*
