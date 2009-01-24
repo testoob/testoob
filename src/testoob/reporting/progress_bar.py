0f2f48fbe1e9c9c52a4936e49a44b6572470b76a
Ronnie van 't Westeinde <Ronnie.van.t.Westeinde@gmail.com>
2009-01-24 16:03:05 +0100

    Made progress bar more attractive

--- src/testoob/reporting/progress_bar.py
+++ src/testoob/reporting/progress_bar.py
@@ -17,25 +17,69 @@
 
 from Tkinter import *
 import threading, sys
+
+class NumberWidget:
+    def __init__(self,master,label,count=0):
+        self.text = Label(master,text=label+' :')
+        self.text.pack(side=LEFT)
+        self.number = Label(master,text=str(count),width=5)
+        self.number.pack(side=LEFT)
+    
+    def set_count(self,count):
+        self.number.configure(text=str(count))
+
+
 class ProgressBarGuiThread(threading.Thread):
-    def __init__(self, num_tests, bar_width):
+    def __init__(self, num_tests):
         threading.Thread.__init__(self)
         self.num_tests = num_tests
-        self.bar_width = bar_width
         self.ready_event = threading.Event()
         self.error = None
-
+        self.bar_state = 'ok'
+        self.num_failures = 0
+        self.num_skipped = 0
+        self.num_tests_run = 0
+        
     def wait_until_ready(self):
         self.ready_event.wait()
+
+    def create_bar_widget(self):
+        self.bar_width = 300
+        self.bar_height = 30
+        self.canvas = Canvas(self.root, height=self.bar_height-2,
+                                        relief='sunken',
+                                        borderwidth=1,
+                                        width=self.bar_width-2)
+        self.canvas.pack()
+        self.progress_bar = self.canvas.create_rectangle(0, 0, 0, self.bar_height, width=0)
+        self.percent_label = self.canvas.create_text(self.bar_width/2, self.bar_height/2, font=('Helvetica', 10, 'bold'))
+    
+    def create_frame(self):
+        self.frame = Frame(self.root)
+        self.frame.pack()
+        
+    def set_state(self,state):
+        if state == 'failed':
+            self.num_failures += 1
+            self.num_tests_run += 1
+            self.bar_state = state
+        elif state == 'skip':
+            self.num_skipped += 1
+        elif state == 'ok':
+            self.num_tests_run += 1
 
     def run(self):
         try:
             try:
                 self.root = Tk()
                 self.count = 0
-                self.textVar = StringVar()
+                self.create_bar_widget()
+                self.create_frame()
+                self.test_cases = NumberWidget(self.root, 'Test Cases',self.num_tests)
+                self.tests_run = NumberWidget(self.root, 'Tests Run')
+                self.failures = NumberWidget(self.root, 'Failures')
+                self.skipped = NumberWidget(self.root, 'Skipped')
                 self.update()
-                Label(self.root, textvariable=self.textVar).pack()
                 self.ready_event.set()
                 self.root.mainloop()
             except TclError:
@@ -44,41 +88,41 @@ class ProgressBarGuiThread(threading.Thread):
             self.ready_event.set()
 
     ratio = property(lambda self: float(self.count) / float(self.num_tests))
-    percent = property(lambda self: "%%%d" % int(100 * self.ratio))
-
-    done_bar_length = property( lambda self: int(self.ratio * self.bar_width) )
-    left_bar_length = property( lambda self: self.bar_width - self.done_bar_length )
-
-    text_bar = property(
-        lambda self: self.done_bar_length * "#" + self.left_bar_length * "_"
-    )
-
-    status = property(lambda self: "%s %s" % (self.text_bar, self.percent))
     
     def advance(self):
         self.count += 1
         self.update()
 
     def die(self):
+        import time
+        time.sleep(1)
         self.root.quit()
 
         # Tk complains when __del__ is called from different thread
-        del self.root, self.textVar
+        del self.root, self.canvas
 
     def update(self):
-        self.textVar.set(self.status)
+        self.canvas.coords(self.progress_bar, 1, 1, self.ratio * self.bar_width, self.bar_height)
+        if self.bar_state in ['ok', 'skip']:
+            color = 'green'
+        else:
+            color = 'red'
+        self.canvas.itemconfigure(self.progress_bar, fill=color)
+        self.canvas.itemconfigure(self.percent_label, text='%d%%'%(self.ratio * 100))
+        self.tests_run.set_count(self.count)
+        self.failures.set_count(self.num_failures)
+        self.skipped.set_count(self.num_skipped)
+        self.tests_run.set_count(self.num_tests_run)
 
+        
 from base import BaseReporter
 class ProgressBarReporter(BaseReporter):
     "Displays a progress bar"
-
-    BAR_WIDTH = 30
 
     def start(self):
         BaseReporter.start(self)
         self._pbg = ProgressBarGuiThread(
             num_tests=self.parameters["num_tests"],
-            bar_width = self.BAR_WIDTH,
         )
         self._pbg.start()
         self._pbg.wait_until_ready()
@@ -91,16 +135,20 @@ class ProgressBarReporter(BaseReporter):
 
     def addError(self, test_info, err_info):
         BaseReporter.addError(self, test_info, err_info)
+        self._pbg.set_state('failed')
         self._pbg.advance()
 
     def addFailure(self, test_info, err_info):
         BaseReporter.addFailure(self, test_info, err_info)
+        self._pbg.set_state('failed')
         self._pbg.advance()
 
     def addSuccess(self, test_info):
         BaseReporter.addSuccess(self, test_info)
+        self._pbg.set_state('ok')
         self._pbg.advance()
 
     def addSkip(self, test_info, err_info, isRegistered=True):
         BaseReporter.addSkip(self, test_info, err_info, isRegistered)
+        self._pbg.set_state('skip')
         self._pbg.advance()
