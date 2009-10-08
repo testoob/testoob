466a6fead92d0ad12b8c0d510d27ccfccd05def3
orip <orip@diamond>
2009-10-08 21:04:32 +0200

    Trying to add IronPython2.6 support to testoob test helper

--- win/runtests.py
+++ win/runtests.py
@@ -7,11 +7,12 @@ def die(msg):
     print>>sys.stderr, msg
     sys.exit(1)
 
-def append_to_pythonpath(path):
-    path_elements = os.environ.get("PYTHONPATH","").split(os.pathsep)
-    os.environ["PYTHONPATH"] = os.pathsep.join(path_elements + [path])
+def append_to_pathvar(varname, path):
+    path_elements = os.environ.get(varname,"").split(os.pathsep)
+    os.environ[varname] = os.pathsep.join(path_elements + [path])
 
-append_to_pythonpath(os.path.join(base_dir, "src"))
+append_to_pathvar('PYTHONPATH', os.path.join(base_dir, "src"))
+append_to_pathvar('IRONPYTHONPATH', os.path.join(base_dir, "src"))
 
 os.environ["TESTOOB_DEVEL_TEST"] = "1"
 
@@ -21,10 +22,11 @@ PYTHON_EXECUTABLES = {
     "2.4" : r"C:\Python24\python.exe",
     "2.3" : r"C:\Python23\python.exe",
     "2.2" : r"C:\Python22\python.exe",
+    "ipy2.6" : r"C:\Program Files\IronPython 2.6\ipy.exe",
 }
 DEFAULT_KEY = "2.6"
 def get_python_executable(args):
-    if len(args) > 0 and re.match("[0-9].[0-9]", args[0]):
+    if len(args) > 0 and args[0] in PYTHON_EXECUTABLES.keys():
         key = args.pop(0) # remove from args list
     else:
         key = DEFAULT_KEY
@@ -33,17 +35,19 @@ def get_python_executable(args):
     result = PYTHON_EXECUTABLES[key]
     if key == "2.2":
         args.append("--color-mode=never") # Python 2.2 pywin32 problems
+    if key == "ipy2.6":
+        append_to_pathvar('IRONPYTHONPATH', r"C:\Python26\Lib")
     if not os.path.exists(result):
         die("Can't find python executable '%s'"%result)
     return result
 
-print "Can specify python version on the commandline, e.g. '%s 2.6'" % (sys.argv[0])
+print "Can specify python version on the commandline, one of %r" % sorted(PYTHON_EXECUTABLES.keys())
+print "Example: '%s 2.6'" % (sys.argv[0])
 args = sys.argv[1:]
 python_executable = get_python_executable(args)
 testoob_executable = os.path.join(base_dir, "src", "testoob", "testoob")
 test_file = os.path.join(base_dir, "tests", "alltests.py")
-extra_args = " ".join(args)
 
-cmdline = "%(python_executable)s %(testoob_executable)s %(test_file)s suite %(extra_args)s" % vars()
-print cmdline
-os.system(cmdline)
+test_args = [python_executable, testoob_executable, test_file, "suite"] + args
+import subprocess
+subprocess.call(test_args)
