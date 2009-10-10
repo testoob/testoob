1c88fe051eb23fc5c9d22b2dff7d0092f68aeaea
orip <orip@diamond>
2009-10-10 13:25:39 +0200

    * better support for "run profiles"  * new '2to3' profile

--- win/runtests.py
+++ win/runtests.py
@@ -16,38 +16,43 @@ append_to_pathvar('IRONPYTHONPATH', os.path.join(base_dir, "src"))
 
 os.environ["TESTOOB_DEVEL_TEST"] = "1"
 
+class Exec:
+    def __init__(self, py_args=None, testoob_extra_args=None):
+        self.py_args = py_args or []
+        self.testoob_extra_args = testoob_extra_args or []
+
 PYTHON_EXECUTABLES = {
-    "2.6" : r"C:\Python26\python.exe",
-    "2.5" : r"C:\Python25\python.exe",
-    "2.4" : r"C:\Python24\python.exe",
-    "2.3" : r"C:\Python23\python.exe",
-    "2.2" : r"C:\Python22\python.exe",
-    "3.1" : r"C:\Python31\python.exe",
-    "3.0" : r"C:\Python30\python.exe",
-    "ipy2.6" : r"C:\Program Files\IronPython 2.6\ipy.exe",
+    "2.6" : Exec([r"C:\Python26\python.exe"]),
+    "2.5" : Exec([r"C:\Python25\python.exe"]),
+    "2.4" : Exec([r"C:\Python24\python.exe"]),
+    "2.3" : Exec([r"C:\Python23\python.exe"]),
+    "2.2" : Exec([r"C:\Python22\python.exe"], ["--color-mode=never"]), # Python 2.2 pywin32 problems
+    "3.1" : Exec([r"C:\Python31\python.exe"]),
+    "3.0" : Exec([r"C:\Python30\python.exe"]),
+    "2to3" : Exec([r"C:\Python26\python.exe", "-3"]),
+    "ipy2.6" : Exec([r"C:\Program Files\IronPython 2.6\ipy.exe"]),
 }
 DEFAULT_KEY = "2.6"
-def get_python_executable(args):
+def get_run_args(args):
+    "get_run_args(args) -> py_args(list), testoob_extra_args(list)"
     if len(args) > 0 and args[0] in PYTHON_EXECUTABLES.keys():
         key = args.pop(0) # remove from args list
     else:
         key = DEFAULT_KEY
     if key not in PYTHON_EXECUTABLES:
         die("unknown version '%s', expecting one of %r" % (key, PYTHON_EXECUTABLES.keys()))
-    result = PYTHON_EXECUTABLES[key]
-    if key == "2.2":
-        args.append("--color-mode=never") # Python 2.2 pywin32 problems
-    if not os.path.exists(result):
-        die("Can't find python executable '%s'"%result)
-    return result
+    exec_info = PYTHON_EXECUTABLES[key]
+    if not os.path.exists(exec_info.py_args[0]):
+        die("Can't find python executable '%s'"%exec_info.py_args[0])
+    return exec_info.py_args, exec_info.testoob_extra_args
 
 print "Can specify python version on the commandline, one of %r" % sorted(PYTHON_EXECUTABLES.keys())
 print "Example: '%s 2.6'" % (sys.argv[0])
 args = sys.argv[1:]
-python_executable = get_python_executable(args)
+py_args, testoob_extra_args = get_run_args(args)
 testoob_executable = os.path.join(base_dir, "src", "testoob", "testoob")
 test_file = os.path.join(base_dir, "tests", "alltests.py")
 
-test_args = [python_executable, testoob_executable, test_file, "suite"] + args
+test_args = py_args + [testoob_executable, test_file, "suite"] + testoob_extra_args
 import subprocess
 subprocess.call(test_args)
