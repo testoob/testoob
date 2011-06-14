5d450f2253755c140b83bfd7e46db23efb5a878f
Ori Peleg <nospam@orip.org>
2011-06-15 01:53:16 +0300

    Added Python 2.7 to tests batch file

--- win/runtestsall.bat
+++ win/runtestsall.bat
@@ -1,5 +1,6 @@
 set PYTHONPATH=src
 set TESTOOB_DEVEL_TEST=1
+c:\Python27\python.exe src\testoob\testoob tests\alltests.py suite %*
 c:\Python26\python.exe src\testoob\testoob tests\alltests.py suite %*
 c:\Python25\python.exe src\testoob\testoob tests\alltests.py suite %*
 c:\Python24\python.exe src\testoob\testoob tests\alltests.py suite %*
