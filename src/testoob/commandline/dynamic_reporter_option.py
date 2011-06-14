4d0ed1241748a6e2bad7c27dcece14ece16d69a1
Ori Peleg <nospam@orip.org>
2011-06-15 02:17:26 +0300

    Fixed ambiguous variable name
    `classname` is confusing next to `class_name`, changed it

--- src/testoob/commandline/dynamic_reporter_option.py
+++ src/testoob/commandline/dynamic_reporter_option.py
@@ -2,14 +2,14 @@ import parsing
 parsing.parser.add_option("--add_reporter", metavar="CLASS",
     help="dynamically load reporter (my_package.my_module.my_reporter_class, must be in the PYTHONPATH)")
 
-def split_module_classname(classname):
-    last_dot = classname.rfind(".")
-    module_name = classname[:last_dot]
-    class_name = classname[last_dot+1:]
+def split_module_classname(full_class_name):
+    last_dot = full_class_name.rfind(".")
+    module_name = full_class_name[:last_dot]
+    class_name = full_class_name[last_dot+1:]
     return module_name, class_name
 
-def dynamically_import_class(classname):
-    module_name, class_name = split_module_classname(classname)
+def dynamically_import_class(full_class_name):
+    module_name, class_name = split_module_classname(full_class_name)
 
     try:
         module = __import__(module_name, globals(), locals(), [class_name])
